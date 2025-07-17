7ae34540f63e049c27f1678d71bae47a930fa1c1
Yogesh15063 <yj15063@gmail.com>
2025-07-17 21:54:45 +0530

    setting backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import Task from "./pages/Task";
-import Onboarding from "./pages/Onboarding"
-import { Routes, Route } from "react-router-dom";
+import Onboarding from "./pages/Onboarding";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
+  const userGoal = localStorage.getItem("selectedGoal");
+
   return (
-  <Routes>
-    <Route path="/"element={<Onboarding/>}/>
-    <Route path="/task"element={<Task/>}/>
-  </Routes>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          userGoal ? <Navigate to="/task" replace /> : <Onboarding />
+        }
+      />
+      <Route path="/task" element={<Task />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
